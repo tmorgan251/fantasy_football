fc4f1f23b5049bd6b2fef2e2ad2d52445486d09a
tmorgan251 <tmorgan251@yahoo.com>
2026-02-03 20:09:10 -0600

    add dependency auto install

--- src/data_fetchers/get_injury_data.py
+++ src/data_fetchers/get_injury_data.py
@@ -1,4 +1,16 @@
 import nfl_data_py as nfl
+import importlib
+import subprocess
+import sys
+
+# Ensure dependencies are installed
+pkg = 'nfl_data_py'
+try:
+    importlib.import_module(pkg)
+except ImportError:
+    print(f"! {pkg} not found. Installing...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", pkg])
+    importlib.invalidate_caches()
 
 # Load injury data for the 2024 season
 # This returns a pandas DataFrame
